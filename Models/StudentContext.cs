a573d990aee87f01e07f29f5290e92cd9e83d6a3
Karina-Abjima <karina.gwalani@abjima.com>
2023-05-15 18:22:39 +0530

    Models/StudentContext.cs and studentapi.metadata.v6.1

---
+++ Models/StudentContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StudentAPI.Models
+{
+    public partial class StudentContext : DbContext
+    {
+        public StudentContext()
+        {
+        }
+
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<StudentDeet> StudentDeets { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ABJIMA-KARINA\\MSSQLSERVER01;Database=Student;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentDeet>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.Contact).HasColumnName("contact");
+
+                entity.Property(e => e.FamilyName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("familyName");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.RollNo).HasColumnName("rollNo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
